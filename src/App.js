a15159a10bc72d33944e80722d9c5a0fc65e9abb
mintah <minka.tahvanainen@knowit.fi>
2023-10-03 11:10:08 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
+  const isAuthenticated = localStorage.getItem('token');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          {!isAuthenticated ? <Login /> : <Redirect to="/dashboard" />}
+        </Route>
+        <Route path="/dashboard">
+          {isAuthenticated ? <Dashboard /> : <Redirect to="/login" />}
+        </Route>
+        <Redirect from="/" to="/login" />
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
